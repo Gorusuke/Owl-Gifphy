504fd79d17b53c2e878023778e7186d28ee459fc
Jose Andres <Gorusuke123@gmail.com>
2021-03-14 01:39:42 -0500

    intentando hacer un test de los componentes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const input = await screen.findByRole('textbox')
+  const button = await screen.findByRole('button')
+
+  fireEvent.change(input, {target: {value: 'Matrix'}})
+  fireEvent.click(button)
+
+  const title = await screen.findByText('Matrix')
+  expect(title).toBeVisible();
+})
